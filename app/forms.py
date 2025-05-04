a0bf7e57f532bc2f45dc5d144b81b8eb8cef891f
SuperMax732 <moltoni.max@gmail.com>
2025-05-04 21:11:18 +0800

    Added LoginForm with username validation

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, BooleanField, SubmitField, StringField
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+import sqlalchemy as sa
+from app import db
+from app.models import User
 
 # List of validators https://wtforms.readthedocs.io/en/2.3.x/validators/
 # Doubt this is correct, just setting the base
@@ -9,3 +12,15 @@ class SignupForm(FlaskForm):
     email = StringField('Email Address', validators=[DataRequired(), Email()])
     password = StringField('Password', validators=[DataRequired(), EqualTo('confirm_password', message='Passwords must match')])
     confirm_password = StringField('Confirm Password', validators=[DataRequired()])
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = StringField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+    # May not be necessary to validate here
+    def validate_username(self, username):
+        user = db.session.scalar(sa.select(User).where(
+            User.username == username.data))
+        if user is None:
+            raise ValidationError('username is Invalid.')
