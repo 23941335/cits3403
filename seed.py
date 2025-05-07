e7aa704699df52ffe9078fc31fe283704f0c00e6
23941335 <168339724+23941335@users.noreply.github.com>
2025-05-07 12:25:49 +0800

    Add hero roles to initial db population script

--- seed.py
+++ seed.py
@@ -1,7 +1,7 @@
 # This script should be run to populate the database with initial values.
 
 from app import app, db
-from app.models import Role
+from app.models import Role, HeroRole
 import sqlalchemy as sa
 
 # Initial population of roles into the database
@@ -19,6 +19,20 @@ def populate_roles():
     db.session.commit()
     print(f'Roles successfully inserted.')
 
+def populate_hero_roles():
+    hero_roles = ['vanguard', 'duelist', 'strategist']
+    for hero_role in hero_roles:
+        existing_role = db.session.scalar(sa.select(HeroRole).where(HeroRole.role_name == hero_role.lower()))
+        if existing_role is None:
+            db.session.add(HeroRole(role_name=hero_role.lower()))
+            print(f'Hero Role {hero_role} will be inserted.')
+        else:
+            print(f'Hero Role {hero_role} already exists in database.')
+    
+    db.session.commit()
+    print(f'Hero Roles successfully inserted.')
+
 if __name__ == '__main__':
     with app.app_context():
         populate_roles()
+        populate_hero_roles()
